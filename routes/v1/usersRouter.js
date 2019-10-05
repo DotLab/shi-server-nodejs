b5fb092dee4f9dbe64ea145847268cdca850ca71
HinataIzayoi <hinataizayoi@gmail.com>
2019-10-04 20:53:08 -0500

    Revert "Add /v1/users/login"
    This reverts commit 7759ce7440be21fd2ede07a9986e18278b1e3eb6.

--- routes/v1/usersRouter.js
+++ routes/v1/usersRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/userController');
-const User = require('../../models/User');
 
 router.post('/register', async (req, res) => {
   const userName = String(req.body.userName);
@@ -13,16 +12,5 @@ router.post('/register', async (req, res) => {
     userName, email, displayName, password,
   }));
 });
-
-
-router.post('/login', async (req, res) => {
-  const email = String(req.body.email);
-  const password = String(req.body.password);
-
-  res.json(await userController.login({
-    email, password,
-  }));
-});
-
 
 module.exports = router;
