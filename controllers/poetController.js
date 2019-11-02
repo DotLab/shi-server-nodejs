63e77a4d3d97cf679266088b4a3a36cc2f71e3d1
hinataizayoi <hinataizayoi@gmail.com>
2019-11-01 23:16:52 -0500

    Add route from profile list poemInfo to profile

--- controllers/poetController.js
+++ controllers/poetController.js
@@ -86,17 +86,17 @@ exports.poems = async function(params) {
     const userId = getUserId(params.token);
     // if user is target user, find all poems
     if (userId === params.poetId) {
-      const poems = await Poem.find({authorId: userId}).exec();
+      const poems = await Poem.find({authorId: userId}).sort({writtenDate: -1}).exec();
       return apiSuccess(poems);
     } else {
       // else find all public and community poems
-      const poems = await Poem.find({authorId: params.poetId, visibility: {$in: [PUBLIC, COMMUNITY]}});
+      const poems = await Poem.find({authorId: params.poetId, visibility: {$in: [PUBLIC, COMMUNITY]}}).sort({writtenDate: -1});
       return apiSuccess(poems);
     }
   }
 
   // if token is invalid, find all public poems
-  const poems = await Poem.find({authorId: params.poetId, visibility: PUBLIC});
+  const poems = await Poem.find({authorId: params.poetId, visibility: PUBLIC}).sort({writtenDate: -1}).exec();
   return apiSuccess(poems);
 };
 
