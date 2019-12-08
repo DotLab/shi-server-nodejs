9974cc20f474bfaadfd0901fadbe3a0f0413b73a
Hinata <hinataizayoi@gmail.com>
2019-12-07 22:19:18 -0600

    Fix comments

--- controllers/queryHandler.js
+++ controllers/queryHandler.js
@@ -8,7 +8,7 @@ const QUERY_ALPHABETICAL = 'alphabetical';
 exports.handleSort = function(sort, order, query) {
   if (sort) {
     if (order !== QUERY_ASC && order !== QUERY_DESC) {
-      throw new Error();
+      throw new Error('Invalid query sort');
     }
   } else {
     query = query.sort({viewCount: -1});
@@ -29,5 +29,5 @@ exports.handleSort = function(sort, order, query) {
     return;
   }
 
-  throw new Error();
+  throw new Error('Invalid input');
 };
